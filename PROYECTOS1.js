ffa5a03f96f7801096515632b1b10b4cc845ff10
GemaVictoria1234 <gema-victoria@hotmail.com>
2021-10-15 16:56:34 -0500

    AREA DEL TRIANGULO

--- PROYECTOS1.js
+++ PROYECTOS1.js
@@ -12,8 +12,24 @@ function onclickButtonCalcularArea(){
     const calcularArea = calcularAreaCuadrado(inputValueArea) 
     const resultado = document.getElementById("result") 
     resultado.innerText = calcularArea 
-    
+
 
 }
 
 
+
+
+
+function calcularAreaTriangulo(base, altura){
+    const area = (base*altura)/2
+    return "EL AREA DEL TRIANGULO ES:  " + area + "cm^2"
+}
+function onclickButtonCalcularAreaTriangulo(){
+    const inputBase = document.getElementById("baseTriangulo")
+    const inputValueBase = inputBase.value
+    const inputAltura = document.getElementById("alturaTriangulo")
+    const inputValueAltura = inputAltura.value
+    const calcularArea = calcularAreaTriangulo(inputValueBase, inputValueAltura)
+    const resultado = document.getElementById("resultTriangulo") 
+    resultado.innerText = calcularArea  
+}
