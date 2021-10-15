b4eefd7b4a36bcd4ab60bcd3c0464bbee586fe06
GemaVictoria1234 <gema-victoria@hotmail.com>
2021-10-15 14:18:02 -0500

    calculo del area del cuadrado en JS

--- PROYECTOS1.js
+++ PROYECTOS1.js
@@ -1,4 +1,19 @@
-console.log("hola Gema Victoria")
-console.log("hola Gema")
+const lado = 10
+const area = lado*lado
+console.log("EL AREA DEL CUADRADO ES: " + area + "cm^2")
+
+function calcularAreaCuadrado(lado){
+    const area = lado*lado
+    return "EL AREA DEL CUADRADO ES : " + area +  "  cm^2"
+}
+function onclickButtonCalcularArea(){
+    const inputArea = document.getElementById("ladoCuadrado")
+    const inputValueArea = inputArea.value 
+    const calcularArea = calcularAreaCuadrado(inputValueArea) 
+    const resultado = document.getElementById("result") 
+    resultado.innerText = calcularArea 
+    
+
+}
 
 
